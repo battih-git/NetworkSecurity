d0353de6612176e75038c434846a962eeb5fb417
Huzefa Battiwala <huzefabattiwala.hb@gmail.com>
2025-10-11 11:07:14 +0530

    data validation

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import sys
 from networksecurity.components.data_ingestion import DataIngestion
 from networksecurity.exception.exception import NetworkSecurityException
 from networksecurity.logging.logger import logging
+from networksecurity.components.data_validation import DataValidation, DataValidationConfig
 from networksecurity.entity.config_entity import DataIngestionConfig, TrainingPipelineConfig
 
 if __name__ == '__main__':
@@ -11,7 +12,15 @@ if __name__ == '__main__':
         data_ingestion = DataIngestion(data_ingestion_config)
         logging.info('Initiate the data ingestion.')
         data_ingestion_artifact = data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation started")
         print(data_ingestion_artifact)
+        data_validation_config = DataValidationConfig(training_pipeline_config)
+        data_validation = DataValidation(data_ingestion_artifact, data_validation_config)
+        logging.info("Initiate the data validation  ")
+        data_validation_artifact = data_validation.initiate_data_validation()
+        logging.info('data validation completed')
+        print(data_ingestion_artifact)
+        
 
     except Exception as e:
         raise NetworkSecurityException(e, sys)
--- networksecurity/constants/training_pipeline/__init__.py
+++ networksecurity/constants/training_pipeline/__init__.py
@@ -7,7 +7,7 @@ import pandas as pd
 Defining constant for training pipeline
 '''
 TARGET_COLUMN = 'Result'
-PIPELIN_NAME:str =  'NetworkSecurity'
+PIPELINE_NAME:str =  'NetworkSecurity'
 ARTIFACT_DIR:str = 'Artifacts'
 FILE_NAME:str = 'NetwrorkData.csv'
 
@@ -23,9 +23,9 @@ Data Ingestion related constant start with DATAINGESTION VAR NAME
 DATA_INGESTION_COLLECTION_NAME: str = 'NetworkData'
 DATA_INGESTION_DATABASE_NAME: str = 'HUZEFA'
 DATA_INGESTION_DIR_NAME: str = 'data_ingestion'
-DATA_INGESTION_FEATURE_STORE_NAME: str = 'feature_store'
-DATA_INGESTION_DIR: str = 'ingested'
-DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO:float = 0.2
+DATA_INGESTION_FEATURE_STORE_DIR: str = 'feature_store'
+DATA_INGESTION_INGESTED_DIR: str = 'ingested'
+DATA_INGESTION_TRAIN_TEST_SPLIT_RATION:float = 0.2
 
 '''
 Data validation related constant start with DATA_VALIDATION VAR NAME
@@ -33,5 +33,5 @@ Data validation related constant start with DATA_VALIDATION VAR NAME
 DATA_VALIDATION_DIR_NAME:str = 'data_validation'
 DATA_VALIDATION_VALID_DIR:str = 'validation'
 DATA_VALIDATION_INVALID_DIR:str = 'invalid'
-DATA_VALIDATION_DRIFT_REPORT:str = 'drift_report'
+DATA_VALIDATION_DRIFT_REPORT_DIR:str = 'drift_report'
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME:str = 'report.yaml'
